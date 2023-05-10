b24d99e82f9357efad7036d4b5631da08227e3e2
ionut <ionut@test.com>
2023-05-10 15:39:45 +0300

    something changed

--- src/firebase/add.js
+++
@@ -1,8 +0,0 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "./firebase";
-
-export const add = async (collectionName, data) => {
-  const docRef = await addDoc(collection(db, collectionName), data);
-
-  return { ...data, id: docRef.id };
-};
